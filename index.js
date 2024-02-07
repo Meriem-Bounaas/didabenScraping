e858094f84a1cfa0c5262faf7d1b2bcb3fdb6655
Meriem BOUNAAS <meriem.bounaas@atos.net>
2024-02-07 16:21:02 +0100

    add products fils csv

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ import puppeteer from "puppeteer";
 import { createArrayCsvWriter } from 'csv-writer'
 
 
-function writeToCsv(categories, filename) {
+function writeToCsv(data, header, filename) {
   const csvWriter = createArrayCsvWriter({
-      header: ['id', 'parent', 'textAr', 'textFr'],
+      header: header,
       path: filename + '.csv'
   });
 
   csvWriter
-      .writeRecords(categories)
+      .writeRecords(data)
       .then(() => console.log('Le fichier CSV "' + filename + '.csv" a été créé avec succès'))
       .catch(error => console.error('Une erreur est survenue lors de la création du fichier CSV :', error));
 }
 
-const getCategories = async () => {
+
+const getData = async () => {
 
   // Start a Puppeteer session 
     const browser = await puppeteer.launch({
@@ -25,11 +26,10 @@ const getCategories = async () => {
   // Open a new page
     const page  = await browser.newPage();
   
-  // - open the URL website with Ar langage
+  // open the URL website with Ar langage
     await page.goto("https://didaben.com/produit", {
         waitUntil: "domcontentloaded",
     });
-
     const CategoriesFr = await page.evaluate(() => {
 
         const categorieList = document.querySelectorAll(".categorie-bar")
@@ -40,15 +40,13 @@ const getCategories = async () => {
             return [categorieTextFr, sousCategorie[0]];
         });
 
-        return categoFr
-        
+        return categoFr 
   });
 
-    // - open the URL website with Ar langage
+  // open the URL website with Fr langage
     await page.goto("https://didaben.com/produit/changeLang", {
         waitUntil: "domcontentloaded",
     });
-
     const CategoriesAr = await page.evaluate(() => {
 
         const categorieList = document.querySelectorAll(".categorie-bar")
@@ -62,44 +60,57 @@ const getCategories = async () => {
         });
 
         return categoAr
+  });
+
+  page.on('console', msg => console.log('CLIENT LOG:', msg.text()));  // pour afficher les logs qui ont dedans evaluate  
+  
+  // open the Products URL website 
+  const productPage  = await browser.newPage();
+  let productLinks = []
+  for (let index = 1; index < 73; index++) {
+    await productPage.goto(`https://didaben.com/produit/rechCatigoSplit/?ser=zafikor&page=${index}`, {
+      waitUntil: "domcontentloaded",
+    });
+    // get links products
+    const itemLinks = await productPage.evaluate(() => {
+        let links = []
+        const linkItems = document.querySelectorAll( "div.product-info h5 strong a")
+        linkItems.forEach(span => {
+            links.push(`https://didaben.com${span.getAttribute("href")}`);
+        });
         
-  });
-
-
-
-
-    // - open the URL website with Ar langage
-    await page.goto("https://didaben.com/produit/rechCatigoSplit/?ser=zafikor", {
-        waitUntil: "domcontentloaded",
+       return links      
     });
 
+    productLinks = [...productLinks,...itemLinks]
+  }
 
+// open the Product detail's URL  
+  const detailProduct  = await browser.newPage();
+  let infoItems = []
+  for (let index = 0; index < productLinks.length; index++) {
 
-  page.on('console', msg => console.log('CLIENT LOG:', msg.text()));  
-  // get product data 
-  const productsFr = await page.evaluate(() => {
-    
-    
-    let itemsInfo = []
-
-    const linkItems = document.querySelectorAll( "div.product-info h5 strong a")
-      linkItems.forEach(span => {
-          itemsInfo.push(`https://didaben.com${span.getAttribute("href")}`);
+    await detailProduct.goto(productLinks[index], {
+      waitUntil: "domcontentloaded",
     });
-    
 
-      // const link = Array.from(hrefItems).map((lien) => {
-      //   lien.click()
-      //   const category = document.querySelector('span').querySelector('.red')
-      //   const reference = document.querySelector('span').querySelector('.green')
+    const infoItem = await detailProduct.evaluate(() => {
+      let infos = []
+      const titreFr = document.querySelector('div.col-lg-4.col-md-4.col-sm-4.product-single-info.full-size h2').innerText
+      const categoryFr = document.querySelector('span.red').innerText
+      const refererence = document.querySelector('span.green').innerText
+      const niveau = document.querySelector('div.col-lg-4.col-md-4.col-sm-4.product-single-info.full-size table tbody tr:nth-of-type(3) td:nth-of-type(2)').innerText
+      const prix = document.querySelector('div.col-lg-4.col-md-4.col-sm-4.product-single-info.full-size span.price').innerText.replace(/,/g, '.')
+      const description = document.querySelector('#style-15')? document.querySelector('#style-15').innerText.replace(/,/g, ';') : ' '
+      const image = document.querySelector('a.fullscreen-button')? document.querySelector('a.fullscreen-button').getAttribute('href') : ' '
 
-      //   itemsInfo.push([category, reference])
-      // });
+      infos.push([titreFr, categoryFr, refererence, niveau, prix, image, description])
+      return infos
+    })
 
-  return itemsInfo      
-  });
+    infoItems = [...infoItem]
+  }
 
-  console.log( productsFr)
 
   let categories = [];
   CategoriesAr.forEach((item, index) => {
@@ -112,12 +123,18 @@ const getCategories = async () => {
     })
   });
 
-  writeToCsv(categories, 'categories')
-  // writeToCsv(products, 'products')
+  let products = [];
+  infoItems.forEach((item, index) => {
+    console.log([index, ...item])
+    products = [...products, [index, ...item]];
+  });
+
+  writeToCsv(categories, ['id', 'parent', 'textAr', 'textFr'],  'categories')
+  writeToCsv(products, ['id', 'titrFr', 'categorieFr', 'ref', 'niveau', 'prix', 'img', 'description'], 'products')
 
   // Close the browser
   await browser.close();
 };
 
 // Start the scraping
-getCategories();
+getData();
